4a88dae932a60902c4c661ad32ffaa7e4dcb3b64
Rene Weber <weber.rene@live.de>
2017-09-10 17:09:12 +0100

    Add js delegation

--- index.js
+++ index.js
@@ -3,4 +3,47 @@ import { NativeModules } from 'react-native';
 
 const { RNTensorflow } = NativeModules;
 
-export default RNTensorflow;
+class Tensorflow {
+
+  setupForModel(modelFileName) {
+    RNTensorflow.instantiateTensorflow(modelFileName)
+  }
+
+  feedWithDims(inputName, src, dims) {
+    RNTensorflow.feed(inputName, src, dims);
+  }
+
+  feed(inputName, src) {
+    RNTensorflow.feed(inputName, src);
+  }
+
+  run(outputNames) {
+    RNTensorflow.run(outputNames);
+  }
+
+  runWithStats(outputNames) {
+    RNTensorflow.run(outputNames, true);
+  }
+
+  fetch(outputName, outputSize) {
+    return RNTensorflow.fetch(outputName, dst);
+  }
+
+  graph() {
+    return RNTensorflow.graph();
+  }
+
+  graphOperation(operationName) {
+    return RNTensorflow.graphOperation(operationName);
+  }
+
+  stats() {
+    return RNTensorflow.getStatString();
+  }
+
+  close() {
+    RNTensorflow.close();
+  }
+}
+
+export default new Tensorflow();
