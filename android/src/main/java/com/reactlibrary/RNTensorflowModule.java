e1458a7219dd5d13438384bac122f70a8c2822e5
Rene Weber <weber.rene@live.de>
2017-09-10 03:03:04 +0100

    Add method stubs

--- android/src/main/java/com/reactlibrary/RNTensorflowModule.java
+++ android/src/main/java/com/reactlibrary/RNTensorflowModule.java
@@ -4,6 +4,8 @@ package com.reactlibrary;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import org.tensorflow.Graph;
+import org.tensorflow.Operation;
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 public class RNTensorflowModule extends ReactContextBaseJavaModule {
@@ -24,5 +26,50 @@ public class RNTensorflowModule extends ReactContextBaseJavaModule {
   @ReactMethod
   public void instantiateTensorflow(String modelFilePath) {
     this.inference = new TensorFlowInferenceInterface(reactContext.getAssets(), modelFilePath);
+  }
+
+  @ReactMethod
+  public void feed(String inputName, double[] src, long[] dims) {
+
+  }
+
+  @ReactMethod
+  public void feed(String inputName, double[] src) {
+
+  }
+
+  @ReactMethod
+  public void run(String[] outputNames) {
+
+  }
+
+  @ReactMethod
+  public void run(String[] outputNames, boolean enableStats) {
+
+  }
+
+  @ReactMethod
+  public void fetch(String outputNames) {
+
+  }
+
+  @ReactMethod
+  public Graph graph() {
+    return this.inference.graph();
+  }
+
+  @ReactMethod
+  public Operation graphOperation(String operationName) {
+    return this.inference.graphOperation(operationName);
+  }
+
+  @ReactMethod
+  public String stats() {
+    return this.inference.getStatString();
+  }
+
+  @ReactMethod
+  public void close() {
+    this.inference.close();
   }
 }
