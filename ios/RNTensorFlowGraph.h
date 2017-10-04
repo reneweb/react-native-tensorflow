2cd5ffcfa4d7789fe931e336fe2a609a2e133c32
Rene Weber <weber.rene@live.de>
2017-10-04 01:45:02 +0100

    Implement graph

--- ios/RNTensorFlowGraph.h
+++ ios/RNTensorFlowGraph.h
@@ -1,9 +1,15 @@
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
+#if __has_include("RCTBridge.h")
+#import "RCTBridge.h"
 #else
-#import <React/RCTBridgeModule.h>
+#import <React/RCTBridge.h>
 #endif
 
+#include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow/core/public/session.h"
+
 @interface RNTensorFlowGraph : NSObject <RCTBridgeModule>
+
+-(void)init:(NSString *)tId graph:(tensorflow::GraphDef)graph;
+-(void)close:(NSString *)tId;
 
 @end
--- ios/RNTensorFlowGraph.mm
+++ ios/RNTensorFlowGraph.mm
@@ -1,57 +1,63 @@
 #import "RNTensorFlowGraph.h"
 
-@implementation RNTensorFlowGraph
+#import "RCTUtils.h"
+
+#include "RNTensorFlowGraphOperations.h"
+
+@implementation RNTensorFlowGraph {
+    std::unordered_map<std::string, tensorflow::GraphDef> graphs;
+}
 
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
 }
+
+- (void)init:(NSString *)tId graph:(tensorflow::GraphDef)graph
+{
+    graphs[[tId UTF8String]] = graph;
+}
+
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(importGraphDef:(NSString *)id graphDef:(NSString *)graphDef)
+RCT_EXPORT_METHOD(importGraphDef:(NSString *)tId graphDef:(NSString *)graphDef)
 {
-    //Graph graph = graphs.get(id);
-    //if(graph != null) {
-    //    graph.importGraphDef(Base64.decode(graphDef, Base64.DEFAULT));
-    //}
+    [self importGraphDefWithPrefix:tId graphDef:graphDef prefix:@""];
 }
 
-RCT_EXPORT_METHOD(importGraphDefWithPrefix:(NSString *)id graphDef:(NSString *)graphDef prefix:(NSString *)prefix)
+RCT_EXPORT_METHOD(importGraphDefWithPrefix:(NSString *)tId graphDef:(NSString *)graphDef prefix:(NSString *)prefix)
 {
-    //Graph graph = graphs.get(id);
-    //if(graph != null) {
-    //    graph.importGraphDef(Base64.decode(graphDef, Base64.DEFAULT), prefix);
-    //}
+    NSData *graphDefDecodedData = [[NSData alloc] initWithBase64EncodedString:graphDef options:0];
+    NSString *graphDefDecodedString = [[NSString alloc] initWithData:graphDefDecodedData encoding:NSUTF8StringEncoding];
+
+    auto graph = graphs.find([tId UTF8String]);
+    if(graph != graphs.end()) {
+        graph->second.ParseFromString([graphDefDecodedString UTF8String]); //prefix??
+    }
 }
 
-RCT_EXPORT_METHOD(toGraphDef:(NSString *)id resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_METHOD(toGraphDef:(NSString *)tId resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-    //Graph graph = graphs.get(id);
-    //if(graph != null) {
-    //    graph.importGraphDef(Base64.decode(graphDef, Base64.DEFAULT));
-    //}
+    auto graph = graphs.find([tId UTF8String]);
+    if(graph != graphs.end()) {
+        resolve(@(graph->second.SerializeAsString().c_str()));
+    } else {
+        reject(RCTErrorUnspecified, @"Could not find graph with given id", nil);
+    }
 }
 
-RCT_EXPORT_METHOD(operation:(NSString *)id name:(NSString *)name resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_METHOD(operation:(NSString *)tId name:(NSString *)name resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-    //try {
-    //  RNTensorFlowGraphOperationsModule operationsModule =
-    //  reactContext.getNativeModule(RNTensorFlowGraphOperationsModule.class);
-    //  operationsModule.init();
-    //  promise.resolve(true);
-    //} catch (Exception e) {
-    //  promise.reject(e);
-    //}
+    reject(RCTErrorUnspecified, @"Unsupported operation", nil);
 }
 
-RCT_EXPORT_METHOD(close:(NSString *)id)
+RCT_EXPORT_METHOD(close:(NSString *)tId)
 {
-    //Graph graph = graphs.get(id);
-    //if(graph != null) {
-    //    graph.close();
-    //}
+    auto graph = graphs.find([tId UTF8String]);
+    if(graph != graphs.end()) {
+        graph->second.Clear();
+        graphs.erase(graph);
+    }
 }
-
 
 @end
-
