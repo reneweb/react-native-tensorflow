cbe9f113e5b2652cdd6600415e456ea1b720ecda
Rene Weber <weber.rene@live.de>
2017-11-08 02:15:49 +0000

    Add missing functionaility to array converter

--- android/src/main/java/com/rntensorflow/converter/ArrayConverter.java
+++ android/src/main/java/com/rntensorflow/converter/ArrayConverter.java
@@ -4,6 +4,9 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableNativeArray;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayConverter {
 
     public static String[] readableArrayToStringArray(ReadableArray readableArray) {
@@ -52,45 +55,70 @@ public class ArrayConverter {
     }
 
     public static byte[] readableArrayToByteBoolArray(ReadableArray readableArray) {
-        byte[] arr = new byte[readableArray.size()];
-        for (int i = 0; i < readableArray.size(); i++) {
-            arr[i] = (byte)(readableArray.getBoolean(i) ? 1 : 0);
+        byte[] bytesArr = new byte[readableArray.size() / 8 + 1];
+        for (int entry = 0; entry < bytesArr.length; entry++) {
+            for (int bit = 0; bit < 8; bit++) {
+                if (readableArray.getBoolean(entry * 8 + bit)) {
+                    bytesArr[entry] |= (128 >> bit);
+                }
+            }
         }
 
-        return arr;
+        return bytesArr;
     }
 
     public static byte[] readableArrayToByteStringArray(ReadableArray readableArray) {
-        byte[] arr = new byte[readableArray.size()];
+        List<Byte> bytes = new ArrayList<>(readableArray.size() * 5);
         for (int i = 0; i < readableArray.size(); i++) {
-            arr[i] = Byte.valueOf(readableArray.getString(i));
+            for (byte b :readableArray.getString(i).getBytes()) {
+                bytes.add(b);
+            }
         }
 
-        return arr;
-    }
-
-    public static ReadableArray stringArrayToReadableArray(String[] arr) {
-        WritableArray writableArray = new WritableNativeArray();
-        for (int i = 0; i < arr.length; i++) {
-            writableArray.pushString(arr[i]);
+        byte[] bytesArr = new byte[bytes.size()];
+        for (int i = 0; i < bytes.size(); i++) {
+            bytesArr[i] = bytes.get(i);
         }
 
-        return writableArray;
+        return bytesArr;
     }
 
     public static ReadableArray doubleArrayToReadableArray(double[] arr) {
         WritableArray writableArray = new WritableNativeArray();
-        for (int i = 0; i < arr.length; i++) {
-            writableArray.pushDouble(arr[i]);
+        for (double d : arr) {
+            writableArray.pushDouble(d);
         }
 
         return writableArray;
     }
 
-    public static ReadableArray longArrayToReadableArray(long[] arr) {
+    public static ReadableArray floatArrayToReadableArray(float[] arr) {
         WritableArray writableArray = new WritableNativeArray();
-        for (int i = 0; i < arr.length; i++) {
-            writableArray.pushDouble(arr[i]);
+        for (float f : arr) {
+            writableArray.pushDouble(f);
+        }
+
+        return writableArray;
+    }
+
+    public static ReadableArray intArrayToReadableArray(int[] arr) {
+        WritableArray writableArray = new WritableNativeArray();
+        for (int i : arr) {
+            writableArray.pushInt(i);
+        }
+
+        return writableArray;
+    }
+
+    public static ReadableArray byteArrayToBoolReadableArray(byte[] arr) {
+        WritableArray writableArray = new WritableNativeArray();
+        byte[] pos = new byte[]{(byte)0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1};
+
+        for(int i = 0; i < arr.length; i++){
+            for(int k = 0; k < 8; k++){
+                boolean res = (arr[i] & pos[k]) != 0;
+                writableArray.pushBoolean(res);
+            }
         }
 
         return writableArray;
